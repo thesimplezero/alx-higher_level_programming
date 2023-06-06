8770ca0bed15dc3e16c3eeadca68b714612a968c
thesimplezero <hi@thesimplezero.io>
2023-06-06 22:14:19 +0300

    edit task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-# Single print function with one loop in a generator expression
-print("".join(chr(i) for i in range(ord('a'), ord('z')+1)), end="")
+# Using a single print function with string format and only one loop
+print("{}".format(''.join(chr(i) for i in range(97, 123))), end="")
