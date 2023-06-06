8985442d14ce1050960263e2770de529820c635d
thesimplezero <hi@thesimplezero.io>
2023-06-06 22:28:31 +0300

    task 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-# Using a single print function with string format and only one loop
 print("{}".format(''.join(chr(i) for i in range(97, 123))), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+print("{}".format(
+    ''.join(
+        chr(i) for i in range(97, 123) 
+        if chr(i) not in {'q', 'e'}
+    )
+), end="")
