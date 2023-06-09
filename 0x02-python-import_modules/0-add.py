c66cc08b982caa08093d729f36ca8143096d1ba9
thesimplezero <hi@thesimplezero.io>
2023-06-09 03:16:08 +0300

    task 0 test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-def main():
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}\n".format(a, b, add(a, b)))
+from add_0 import add
 
+a = 1
+b = 2
 
-if __name__ == "__main__":
-    main()
+result = add(a, b)
+print(f"{a} + {b} = {result}")
