a4efe876e8f66b87357481d2da06de306c410490
thesimplezero <hi@thesimplezero.io>
2023-06-09 03:03:50 +0300

    fixed task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """print sum of 1 and 2."""
+def main():
     from add_0 import add
-
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}\n".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
