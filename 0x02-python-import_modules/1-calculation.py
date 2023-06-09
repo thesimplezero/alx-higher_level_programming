e16714ed28770376ae47e22723ea00cb734410a1
thesimplezero <hi@thesimplezero.io>
2023-06-09 03:30:29 +0300

    task 1 fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
-    result_add = add(a, b)
-    print(f"{a} + {b} = {result_add}")
-
-    result_sub = sub(a, b)
-    print(f"{a} - {b} = {result_sub}")
-
-    result_mul = mul(a, b)
-    print(f"{a} * {b} = {result_mul}")
-
-    result_div = div(a, b)
-    print(f"{a} / {b} = {result_div}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
