de48635ddc116024d8ed13c98ec5887784ef31ef
thesimplezero <hi@thesimplezero.io>
2023-06-27 02:58:10 +0300

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,20 @@
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
+    """Prints x elements from my_list safely"""
+    if my_list is None:
+        my_list = []
 
-  counter = 0
-  for i in range(x):
-    try:
-      print(my_list[i], end="")
-      counter += 1
-    except IndexError:
-      break
-
-  print()
-  return counter
+    counter = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            counter += 1
+        except IndexError:
+            break
+    print()
+    return counter
 
 
 if __name__ == "__main__":
-  my_list = [1, 2, 3, 4, 5]
-  print(safe_print_list(my_list, 3))
-  print(safe_print_list(my_list, 10))
+    my_list = [1, 2, 3, 4, 5]
+    print(safe_print_list(my_list, 3))
+    print(safe_print_list(my_list, 10))
