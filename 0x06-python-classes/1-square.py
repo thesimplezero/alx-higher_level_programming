20f2aada09bf709283ead7601adbf9c9bb9a5fd4
thesimplezero <hi@thesimplezero.io>
2023-06-28 00:40:00 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defining Square class, representing a geometric square."""
+
+
+class Square:
+    """Represents a geometric square, characterized by its size."""
+
+    def __init__(self, size):
+        """Initialize square with a given size."""
+        self.__size = size
