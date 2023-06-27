38bc9f5ae10179b8cb89919d56ba1446a3a12668
thesimplezero <hi@thesimplezero.io>
2023-06-28 01:22:08 +0300

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square representing a geometric square with size 
+and position attributes.
+"""
+
+
+class Square:
+    """
+    Represent a geometric square. Allows setting of size and position with 
+    validations. Provides methods to calculate the area and print the square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialize a new square with given size and position.
+        Args:
+            size (int): The size of the new square.
+            position (tuple): The position of the new square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the current position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position with validation."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate and return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using the '#' character."""
+        if self.__size == 0:
+            print("")
+        else:
+            print("\n" * self.__position[1], end="")
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
