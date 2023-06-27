3c4f02bd202e9928ede8bb0efa717e8c19508a89
thesimplezero <hi@thesimplezero.io>
2023-06-27 03:27:18 +0300

    Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Raises a NameError with a specified message.
+
+    Args:
+    message: The message to be associated with the NameError exception.
+    """
+    raise NameError(message)
