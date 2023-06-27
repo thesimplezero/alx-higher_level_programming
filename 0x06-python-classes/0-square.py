de713ad94ff36826d0c43231e7c662f6cb55f4ba
thesimplezero <hi@thesimplezero.io>
2023-06-28 00:33:30 +0300

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module defining the Square class.
+
+This module provides a simple and clear implementation
+of a square, which could be used as a blueprint for
+more complex geometric shapes.
+"""
+
+
+class Square:
+    """
+    Represent a square.
+
+    This class represents a square in a geometric context.
+    Currently, it's an empty class and serves as a structure
+    to be expanded later with properties like side length, area, etc.
+    """
+    pass
