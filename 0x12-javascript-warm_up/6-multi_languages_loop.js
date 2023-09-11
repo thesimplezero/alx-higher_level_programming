e39e2fe350d019ae6edecd2b96657f5b9a201715
thesimplezero <hi@thesimplezero.io>
2023-09-11 22:21:17 +0300

    print sentences using a lopp

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each element
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
+}
