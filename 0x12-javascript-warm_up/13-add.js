2f6aef771a31984320eb8c8c98d0333bdd401eb6
thesimplezero <hi@thesimplezero.io>
2023-09-11 22:49:47 +0300

    returns the addition of two integers

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Define a function named "add" that takes two parameters and returns their sum
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Export the "add" function to make it visible from outside
+  exports.add = add;
+  
