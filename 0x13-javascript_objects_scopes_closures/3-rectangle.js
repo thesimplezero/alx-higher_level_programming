7b039ce00a4d1346e3f8c4e646c65ddaafa2fc22
thesimplezero <hi@thesimplezero.io>
2023-09-12 21:59:43 +0300

    print a square

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        Object.create(null); // Create an empty object
+      }
+    }
+  
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  };
+  
