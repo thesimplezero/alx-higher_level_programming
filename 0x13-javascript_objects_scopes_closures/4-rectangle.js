5fc4e8751ab8955b33ce292dd19313553b909c0a
thesimplezero <hi@thesimplezero.io>
2023-09-12 22:02:47 +0300

    rotate then double it

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object for invalid dimensions
+        return Object.create(null);
+      }
+    }
+  
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  
+    rotate() {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  };
+  
