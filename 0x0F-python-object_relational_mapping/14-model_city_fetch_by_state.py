0c76a853b4016aa6f9e8a112b361a899b6ff8295
thesimplezero <hi@thesimplezero.io>
2023-09-17 19:51:23 +0300

    Refactor database connection and query for better readability.

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 """
-Module that connects python script to a database
+Module that connects a python script to a database
 """
 from sys import argv
 from sqlalchemy import create_engine
@@ -9,18 +8,23 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-
+def main():
+    # Create a database connection using command-line arguments
+    db_username, db_password, db_name = argv[1], argv[2], argv[3]
     engine = create_engine(
-    f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
-    pool_pre_ping=True
+        f'mysql+mysqldb://{db_username}:{db_password}@localhost/{db_name}',
+        pool_pre_ping=True
     )
-   
-    my_session_maker = sessionmaker(bind=engine)
-    my_session = my_session_maker()
 
-    for state, city in my_session.query(State, City).filter(
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and print state-city pairs
+    for state, city in session.query(State, City).filter(
             State.id == City.state_id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+        print(f"{state.name}: ({city.id}) {city.name}")
 
-    my_session.close()
+    session.close()
+
+if __name__ == "__main__":
+    main()
