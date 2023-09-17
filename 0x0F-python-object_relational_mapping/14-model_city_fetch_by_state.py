1649aa5b1553fad9e98692de5db7d6a31f000240
thesimplezero <hi@thesimplezero.io>
2023-09-17 19:45:07 +0300

    Add database connection and data retrieval

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module that connects python script to a database
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+    f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
+    pool_pre_ping=True
+    )
+   
+    my_session_maker = sessionmaker(bind=engine)
+    my_session = my_session_maker()
+
+    for state, city in my_session.query(State, City).filter(
+            State.id == City.state_id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    my_session.close()
