25bb303cd70df327a535cd6b311c504affa96b18
thesimplezero <hi@thesimplezero.io>
2023-09-28 15:34:58 +0300

    fix feature

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Use cURL to send a request to the URL and save the response body to a temporary file
-response=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
-echo "$response"
+#Takes in a URL and send a request to it
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
