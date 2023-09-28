2b9980176e80f8dacc6f8baa636fb2bce04f2f04
thesimplezero <hi@thesimplezero.io>
2023-09-28 15:51:36 +0300

    feature:  take in a URL, send a GET request to the URL, and display the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Use cURL to send a GET request to the URL and display the body if the response code is 200
-response_code=$(curl -s -o /dev/null -w "%{http_code}" -L "$1")
-if [ "$response_code" -eq 200 ]; then
-    curl -s -L "$1"
-else
-    echo "Response code: $response_code"
-fi
+# Get request to the URL and display the body of the response
+curl -sL "$1"
