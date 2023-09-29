0cb0986951bb5e0d6ad0c4c3b3a3784d005cdb23
thesimplezero <hi@thesimplezero.io>
2023-09-30 01:04:26 +0300

    feature:     Fetches and displays the latest commits from a GitHub repository.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""using the GitHub API to list commits"""
+"""using GitHub API to list commits"""
 
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    # get the repo name and owner from the command line
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    # GitHub API URL
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
         owner_name, repository_name)
 
-    # limit no of pages
     params = {'per_page': 10}
 
     # send a GET request to the GitHub API
