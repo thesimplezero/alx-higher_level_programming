a8033c15d7b882fc2900d4ffc121e49a23cf9323
thesimplezero <hi@thesimplezero.io>
2023-10-24 21:02:19 +0300

    edit completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
 
+// Import the module
 const request = require('request');
 
-// Get the API URL from the command-line arguments
 const apiUrl = process.argv[2];
 
-// Create an empty dictionary to store user counts
-const userCounts = {};
+const dictionary = {};
 
-// Make an HTTP GET request to the API URL
-request(apiUrl, (error, response, body) => {
+request(apiUrl, function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
+    console.error(error);
   } else {
-    // Parse the JSON response
     const data = JSON.parse(body);
-
-    // Iterate through the data and count completed tasks per user
-    data.forEach((result) => {
+    data.forEach(function (result) {
       if (result.completed === true) {
-        const userId = result.userId;
-        userCounts[userId] = (userCounts[userId] || 0) + 1;
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
+        }
+        dictionary[userid] += 1;
       }
     });
-
-    console.log(userCounts);
+    console.log(dictionary);
   }
 });
